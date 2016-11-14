319a87dbab3fe5c9edb0b6934412802fa2547a66
gvoz <gvozdmesiter@gmail.com>
2016-11-14 22:43:58 +0300

    log all messages

--- lib/gitterlog/log.rb
+++ lib/gitterlog/log.rb
@@ -13,16 +13,22 @@ module GitterLog
 
     def save_room_messages(roomId)
       date_message = ''
-      @client.messages(roomId).each do |x|
-        if date_message == Date.parse(x["sent"]).strftime("%d_%m_%Y")
-          mode = 'a'
-        else
-          date_message = Date.parse(x["sent"]).strftime("%d_%m_%Y")
-          mode = 'w'
+      before_id = ''
+      messages = @client.messages(roomId, {beforeId: before_id})
+      until messages.empty? do
+        before_id = messages[0]["id"]
+        messages.each do |x|
+          if date_message == Date.parse(x["sent"]).strftime("%d_%m_%Y")
+            mode = 'a'
+          else
+            date_message = Date.parse(x["sent"]).strftime("%d_%m_%Y")
+            mode = 'w'
+          end
+          f = File.open("logs/#{roomId}_#{date_message}.txt", mode)
+          f.puts "#{x["fromUser"]["username"]}: #{x["text"]}"
+          f.close
         end
-        f = File.open("logs/#{roomId}_#{date_message}.txt", mode)
-        f.puts "#{x["fromUser"]["username"]}: #{x["text"]}"
-        f.close
+        messages = @client.messages(roomId, {beforeId: before_id})
       end
     end
 
