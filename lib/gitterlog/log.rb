680deef08c00339bc140b4e8ebf12c7f10f6c0cd
gvoz <gvozdmesiter@gmail.com>
2016-11-15 15:48:49 +0300

    Take all messages in room

--- lib/gitterlog/log.rb
+++ lib/gitterlog/log.rb
@@ -1,35 +1,59 @@
 module GitterLog
 
   class Log
+    # Set the all messages in room
+    attr_accessor :all_messages
+
     # Set the client
     attr_accessor :client
 
     def initialize(client = Client.new)
+      if Dir["logs"].empty?
+        Dir.mkdir("logs")
+      end
       @client = client
       @client.rooms.each do |x|
-        save_room_messages(x["id"])
+        @all_messages = []
+        get_room_messages(x["id"])
+        save_files(x["id"])
       end
     end
 
-    def save_room_messages(roomId)
+    private
+
+    def get_room_messages(roomId)
       date_message = ''
       before_id = ''
       messages = @client.messages(roomId, {beforeId: before_id})
       until messages.empty? do
         before_id = messages[0]["id"]
-        messages.each do |x|
-          if date_message == Date.parse(x["sent"]).strftime("%d_%m_%Y")
-            mode = 'a'
-          else
-            date_message = Date.parse(x["sent"]).strftime("%d_%m_%Y")
-            mode = 'w'
+        messages.reverse_each do |m|
+          unless date_message == date_parse(m["sent"])
+            date_message = date_parse(m["sent"])
+            # exit if file exist
+            return unless Dir["logs/#{roomId}_#{date_message}.txt"].empty?
           end
-          f = File.open("logs/#{roomId}_#{date_message}.txt", mode)
-          f.puts "#{x["fromUser"]["username"]}: #{x["text"]}"
-          f.close
+          # not log today messages
+          unless date_message == DateTime.now.strftime("%Y_%m_%d")
+            @all_messages << [date_message, "#{m["fromUser"]["username"]}: #{m["text"].delete("\n")}"]
+          end
         end
-        messages = @client.messages(roomId, {beforeId: before_id})
+          messages = @client.messages(roomId, {beforeId: before_id})
       end
+    end
+
+    def save_files(roomId)
+      date_message = ''
+      @all_messages.reverse_each do |m|
+        unless date_message == m[0]
+          date_message = m[0]
+        end
+        File.open("logs/#{roomId}_#{date_message}.txt", 'a') { |f| f.puts "#{m[1]}"}
+      end
+    end
+
+    def date_parse(date)
+      Date.parse(date).strftime("%Y_%m_%d")
     end
 
   end
